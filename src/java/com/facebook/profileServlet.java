b7adfbe3586597b1137516408ebc1ee64f1d1ff1
sahil <sahilmujawar.seti@gmail.com>
2022-04-06 13:37:55 +0530

    My First Commit

---
+++ src/java/com/facebook/profileServlet.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.facebook;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author exam
+ */
+public class profileServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet profileServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            
+             request.getRequestDispatcher("link.html").include(request, response);
+            
+            Cookie cookie[]=request.getCookies();
+            //String name=cookie[0].getValue();
+           // out.print(name);
+            //out.print("line 47");
+            
+            if(cookie!=null)
+            {
+                //out.print("line 51");
+            
+                String name=cookie[0].getValue();
+                //out.print("line 54");
+            
+                if(!name.equals(""))
+                {
+                  //  out.print("line 58");
+            
+                    out.print("<h1> Well come to Profile </h1>");
+                    out.print("<br><h2>Well come "+name+"</h2>");
+                }
+            }else
+            {
+                out.println("First You have to login!!!");
+                request.getRequestDispatcher("login.html").include(request, response);
+            }
+            
+            
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
